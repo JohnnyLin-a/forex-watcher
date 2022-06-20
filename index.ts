d94916a38f727198b553db1b11876219739ab989
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2022-06-20 02:12:40 +0000

    Add loading config logic

--- index.ts
+++ index.ts
@@ -1,3 +1,28 @@
 /**
  * Entrypoint to this app.
  */
+
+// Interfaces
+interface CurrencyConfig {
+    notifyOver: number | null
+    notifyBelow: number | null
+    from: string
+    to: string
+}
+
+interface GlobalConfig {
+    discordWebhook: string
+    currencies: CurrencyConfig[]
+}
+
+// load env if needed?
+const config: GlobalConfig = process.env.FOREX_WATCHER_CONFIG
+    ? JSON.parse(process.env.FOREX_WATCHER_CONFIG)
+    : require("forex-watcher-config.json")
+
+// fetch forex data
+config.currencies.forEach((currencyConfig) => {
+    
+})
+
+// notify discord according to config
