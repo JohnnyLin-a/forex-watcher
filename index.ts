fcdab0aef9e66fd18c7125bad920bff9d930d4c0
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2022-06-20 02:34:05 +0000

    Add logic

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@
  * Entrypoint to this app.
  */
 
+import axios from "axios"
+import { setTimeout } from "timers/promises"
+
 // Interfaces
 interface CurrencyConfig {
     notifyOver: number | null
@@ -12,17 +15,39 @@ interface CurrencyConfig {
 
 interface GlobalConfig {
     discordWebhook: string
+    discordMention: string
     currencies: CurrencyConfig[]
 }
 
 // load env if needed?
 const config: GlobalConfig = process.env.FOREX_WATCHER_CONFIG
     ? JSON.parse(process.env.FOREX_WATCHER_CONFIG)
-    : require("forex-watcher-config.json")
+    : require("./forex-watcher-config.json")
 
 // fetch forex data
-config.currencies.forEach((currencyConfig) => {
-    
-})
+;(async () => {
+    for (let currencyConfig of config.currencies) {
+        await setTimeout(3000)
+        const response = await axios.get(
+            `https://wise.com/rates/live?source=${currencyConfig.from}&target=${currencyConfig.to}`,
+            {
+                headers: {
+                    "User-Agent":
+                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+                },
+            }
+        )
 
-// notify discord according to config
+        // notify discord according to config
+        if (
+            (currencyConfig.notifyBelow &&
+                response.data.value <= currencyConfig.notifyBelow) ||
+            (currencyConfig.notifyOver &&
+                response.data.value >= currencyConfig.notifyOver)
+        ) {
+            await axios.post(config.discordWebhook, {
+                content: `${config.discordMention} 1${currencyConfig.from} = ${response.data.value}${currencyConfig.to}`,
+            })
+        }
+    }
+})()
