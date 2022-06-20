603547449335ff243c0b04ec26f54d1eeaa48e5e
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2022-06-20 02:10:57 +0000

    Remove need for postgres

--- dev_ts.sh
+++ dev_ts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -it -v $PWD:/src -w /src --env-file postgres.env --network host node:18-alpine sh
+docker run --rm -it -v $PWD:/src -w /src --network host node:18-alpine sh
