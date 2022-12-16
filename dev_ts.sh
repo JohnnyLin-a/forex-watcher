78f4ecfbe6bc6d3ccc074135cc730bb33b673275
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2022-12-16 20:17:10 +0000

    Update dev

--- dev_ts.sh
+++ dev_ts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -it -v $PWD:/src -w /src --network host node:18-alpine sh
+docker run --name dev-forex-watcher --rm -d --entrypoint "tail" -v $PWD:/src -w /src node:18-bullseye-slim "-f" "/dev/null"
